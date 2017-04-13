87a3ac3c8d3b8c459509f23f211caa99cf0315d1
kaishui <kaishuinanhai@gmail.com>
2017-04-13 19:18:21 +0800

    增加维度信息

--- py_mlh_scrapy/items.py
+++ py_mlh_scrapy/items.py
@@ -17,17 +17,25 @@ class ListItem(scrapy.Item):
 
 class DetailItem(scrapy.Item):
     # define the fields for your item here like:
-    title = Field(serializer=str)         # 标题
-    originUrl = Field(serializer=str)     # 来源
-    content = Field(serializer=str)       # 内容
-    imgs = Field()          # 图片urls
-    tags = Field()           # 标签
-    createUser = Field(serializer=str)    # 作者
-    createTime = Field(serializer=str)    # 创建时间
-    category = Field()      # 分类
+    title = Field(serializer=str)           # 标题
+    url = Field(serializer=str)             # 来源
+    content = Field(serializer=str)         # 内容
+    tags = Field()                          # 标签
+    createUser = Field(serializer=str)      # 作者
+    createTime = Field(serializer=str)      # 创建时间
+    category = Field()                      # 分类
+    location = Field()                      # 经维度 {""}
+    orginImgs = Field()                     # 原图片 [{orgin:"image url", copyright："版权信息"}]
+    targetImgs = Field()                    # 转化成目标图片urls [{orgin:"来源image url", target:"转化成阿里云oss url", copyright:"版权信息"}]
 
 
 # 维度
 class Demension(scrapy.Item):
     attr = Field(serializer=str)         # 属性
     text = Field(serializer=str)         # 值
+
+# 图片信息
+class ImgeItem(scrapy.Item):
+    orgin = Field(serializer=str)       # 来源
+    target = Field(serializer=str)      # 阿里云oss image url
+    copyright = Field(serializer=str)   # 版权信息
